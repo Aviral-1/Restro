34bf8273f8add6cbfeae8119574f78d6e7fa77a0
Aviral-1 <aviralsul2000@gmail.com>
2026-03-25 09:09:12 +0530

    gujduyfgdxuy

--- backend/nest-api/src/seed.ts
+++ backend/nest-api/src/seed.ts
@@ -70,7 +70,7 @@ async function seed() {
       limits: { maxItems: 100000, maxOrders: 100000, maxTables: 1000, hasAnalytics: true, hasCustomBranding: true },
     },
   ]);
-  console.log('✅ Subscription Plans created ');
+  console.log('✅ Subscription Plans created dghsdrfghdf hdfhdf hdfhtfyhdtghysdryhg ');
 
   // === USERS ===
   const hashedPassword = await bcrypt.hash('password123', 12);
