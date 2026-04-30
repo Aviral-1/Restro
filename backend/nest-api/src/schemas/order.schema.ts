823d0bf6127f51f647a56c180b9cd6cfd8ec56a6
Aviral-1 <aviralsul2000@gmail.com>
2026-05-01 04:32:35 +0530

    Fix: Improve MongoDB connection logging and use 127.0.0.1 i iuguio v

--- backend/nest-api/src/schemas/order.schema.ts
+++ backend/nest-api/src/schemas/order.schema.ts
@@ -40,7 +40,7 @@ export class Order extends Document {
   @Prop({ required: true, unique: true })
   orderNumber: string;
 
-  @Prop({ type: Types.ObjectId, ref: 'Restauranthfjyflf eryd yedydryrsdydrsekl;jhpiohpohpohpoihpohpohophpohpohrgergdfgdrgdfghdfghrfstysdrtysdrysdftfsdtgdftdftsdftsdftgsdfgsdfgsdfgsdfgxfcghs hgvjhgvhujvgjhrf3rf3rf3rf3rf3fhdfhdfhdfhdfhdfhdfhfdhdfh iughoiugpiuogpuigpiug fgbfdfgdfg ythythththyh thntryn dhdfhd fhdh fhbxfh xdfh tyntyntyn ty sfgs sdgsdfgsfghsfgsfdgsdf xs xdflkufkuf dghdsf xgbxcgbcxbcx ghsdrhgfgh fhgdf hdfhdfhdfhdfh dfhgfsdhfdhdfhdfhdfhdfh rtsrtsrtsrts ertsrtsdftg dftgsd gsdgsd  wtgwetg wsg dfghd fhdfhdfhd zdfsad fgsfdgdrfhgdfhdfhdfhdfhfdhfd dfhdjfjkdfsdghd fhdhdhdhdhdhdfg dsdhdfhdfhdfhdfh kjglkfui il fgysdf hsfhdfhdfhdf hdfghdf hdfhdfdfgbf sddhshedfhdf gfhdgh dh gchdcghdg jhdg fdghdfg sdfg djh', required: true })
+  @Prop({ type: Types.ObjectId, ref: 'Restauranthfjyflf  gwergrwgwrg rgwergrwegergerg egerger gereryd yedydryrsdydrsekl;jhpiohpohpohpoihpohpohophpohpohrgergdfgdrgdfghdfghrfstysdrtysdrysdftfsdtgdftdftsdftsdftgsdfgsdfgsdfgsdfgxfcghs hgvjhgvhujvgjhrf3rf3rf3rf3rf3fhdfhdfhdfhdfhdfhdfhfdhdfh iughoiugpiuogpuigpiug fgbfdfgdfg ythythththyh thntryn dhdfhd fhdh fhbxfh xdfh tyntyntyn ty sfgs sdgsdfgsfghsfgsfdgsdf xs xdflkufkuf dghdsf xgbxcgbcxbcx ghsdrhgfgh fhgdf hdfhdfhdfhdfh dfhgfsdhfdhdfhdfhdfhdfh rtsrtsrtsrts ertsrtsdftg dftgsd gsdgsd  wtgwetg wsg dfghd fhdfhdfhd zdfsad fgsfdgdrfhgdfhdfhdfhdfhfdhfd dfhdjfjkdfsdghd fhdhdhdhdhdhdfg dsdhdfhdfhdfhdfh kjglkfui il fgysdf hsfhdfhdfhdf hdfghdf hdfhdfdfgbf sddhshedfhdf gfhdgh dh gchdcghdg jhdg fdghdfg sdfg djh', required: true })
   restaurantId: Types.ObjectId;
 
   @Prop()
